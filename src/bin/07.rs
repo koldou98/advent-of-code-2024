baf01ee0d414998f09ac30e6447878e0ba8afb96
koldou98 <48926759+koldou98@users.noreply.github.com>
2024-12-07 07:34:23 +0100

    Day 7 - refactor code

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,8 +1,21 @@
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<usize> {
+    let concatenation = false;
     let operations = map_content(input);
-    let result: usize = operations
+    let result: usize = get_calibration_result(operations, concatenation);
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let concatenation = true;
+    let operations = map_content(input);
+    let result: usize = get_calibration_result(operations, concatenation);
+    Some(result)
+}
+
+fn get_calibration_result(operations: Vec<(usize, Vec<usize>)>, concatenation: bool) -> usize {
+    operations
         .iter()
         .filter_map(|(result, numbers)| {
             if numbers.len() == 1 && result == numbers.first().unwrap() {
@@ -10,24 +23,10 @@ pub fn part_one(input: &str) -> Option<usize> {
             } else {
                 let mut operation_result = vec![];
                 for i in 1..numbers.len() {
-                    let left_num = numbers[i - 1];
-                    let right_num = numbers[i];
                     if operation_result.is_empty() {
-                        operation_result.push(left_num * right_num);
-                        operation_result.push(left_num + right_num);
+                        apply_first_operation(i, numbers, &mut operation_result, concatenation);
                     } else {
-                        for j in (0..operation_result.len()).rev() {
-                            let mult_value = operation_result[j] * numbers[i];
-                            if mult_value <= *result {
-                                operation_result.push(mult_value);
-                            }
-                            let sum_value = operation_result[j] + numbers[i];
-                            if sum_value > *result {
-                                operation_result.remove(j);
-                            } else {
-                                operation_result[j] = sum_value;
-                            }
-                        }
+                        apply_operations(i, numbers, &mut operation_result, concatenation, result);
                     }
                 }
                 if operation_result.iter().any(|element| element == result) {
@@ -36,8 +35,51 @@ pub fn part_one(input: &str) -> Option<usize> {
             }
             None
         })
-        .sum();
-    Some(result)
+        .sum()
+}
+
+fn apply_first_operation(
+    index: usize,
+    numbers: &[usize],
+    operation_result: &mut Vec<usize>,
+    concatenation: bool,
+) {
+    let left_num = numbers[index - 1];
+    let right_num = numbers[index];
+    operation_result.push(left_num * right_num);
+    operation_result.push(left_num + right_num);
+    if concatenation {
+        let string = left_num.to_string() + &right_num.to_string();
+        operation_result.push(string.parse().unwrap());
+    }
+}
+
+fn apply_operations(
+    index: usize,
+    numbers: &[usize],
+    operation_result: &mut Vec<usize>,
+    concatenation: bool,
+    result: &usize,
+) {
+    for j in (0..operation_result.len()).rev() {
+        if concatenation {
+            let string_value = operation_result[j].to_string() + &numbers[index].to_string();
+            let usize_string_value = string_value.parse::<usize>().unwrap();
+            if usize_string_value <= *result {
+                operation_result.push(usize_string_value);
+            }
+        }
+        let mult_value = operation_result[j] * numbers[index];
+        if mult_value <= *result {
+            operation_result.push(mult_value);
+        }
+        let sum_value = operation_result[j] + numbers[index];
+        if sum_value > *result {
+            operation_result.remove(j);
+        } else {
+            operation_result[j] = sum_value;
+        }
+    }
 }
 
 fn map_content(input: &str) -> Vec<(usize, Vec<usize>)> {
@@ -55,55 +97,6 @@ fn map_content(input: &str) -> Vec<(usize, Vec<usize>)> {
             (result, numbers)
         })
         .collect::<Vec<(usize, Vec<usize>)>>()
-}
-
-pub fn part_two(input: &str) -> Option<usize> {
-    let operations = map_content(input);
-    let result: usize = operations
-        .iter()
-        .filter_map(|(result, numbers)| {
-            if numbers.len() == 1 && result == numbers.first().unwrap() {
-                return Some(*result);
-            } else {
-                let mut operation_result = vec![];
-                for i in 1..numbers.len() {
-                    let left_num = numbers[i - 1];
-                    let right_num = numbers[i];
-                    if operation_result.is_empty() {
-                        operation_result.push(left_num * right_num);
-                        operation_result.push(left_num + right_num);
-                        let string = left_num.to_string() + &right_num.to_string();
-                        operation_result.push(string.parse().unwrap());
-                    } else {
-                        for j in (0..operation_result.len()).rev() {
-                            let string_value =
-                                operation_result[j].to_string() + &numbers[i].to_string();
-                            let usize_string_value = string_value.parse::<usize>().unwrap();
-                            if usize_string_value <= *result {
-                                operation_result.push(usize_string_value);
-                            }
-
-                            let mult_value = operation_result[j] * numbers[i];
-                            if mult_value <= *result {
-                                operation_result.push(mult_value);
-                            }
-                            let sum_value = operation_result[j] + numbers[i];
-                            if sum_value > *result {
-                                operation_result.remove(j);
-                            } else {
-                                operation_result[j] = sum_value;
-                            }
-                        }
-                    }
-                }
-                if operation_result.iter().any(|element| element == result) {
-                    return Some(*result);
-                }
-            }
-            None
-        })
-        .sum();
-    Some(result)
 }
 
 #[cfg(test)]
