b8f8eeb60bec342427746e99dedf1356abb45e24
koldou98 <48926759+koldou98@users.noreply.github.com>
2024-12-10 21:20:43 +0100

    refactor(day-10): use the same logic for part 1 and 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,78 +1,57 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
 advent_of_code::solution!(10);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut point_list = Vec::new();
-    let max_y = input.lines().count();
-    let max_x = input.lines().next().unwrap().chars().count();
-    let map: Vec<Vec<isize>> = parse_input(input, &mut point_list);
-    let mut result = 0;
-    let mut seen = HashSet::new();
-
-    for point in point_list {
-        seen.clear();
-        let mut points_to_check = VecDeque::from([point]);
-        while !points_to_check.is_empty() {
-            let point = points_to_check.pop_front().unwrap();
-            if seen.contains(&point) {
-                continue;
-            }
-            seen.insert(point);
-            if map[point.0 as usize][point.1 as usize] == 9 {
-                result += 1;
-            } else {
-                for (y1, x1) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
-                    let new_y = point.0 + y1;
-                    let new_x = point.1 + x1;
-
-                    if in_bounds(point, &map, max_x, max_y, new_y, new_x) {
-                        points_to_check.push_back((new_y, new_x));
-                    }
-                }
-            }
-        }
+    let mut starting_locations = Vec::new();
+    let map: Vec<Vec<isize>> = parse_input(input, &mut starting_locations);
+    let mut trailheads = Vec::new();
+    for point in starting_locations {
+        let hills = get_trailheads(&map, point);
+        trailheads.push(hills);
     }
+    let result = trailheads
+        .iter()
+        .map(|trailhead| trailhead.iter().collect::<HashSet<_>>().len() as u32)
+        .sum();
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut point_list = Vec::new();
-    let max_y = input.lines().count();
-    let max_x = input.lines().next().unwrap().chars().count();
-    let map: Vec<Vec<isize>> = parse_input(input, &mut point_list);
-    let mut result = 0;
-    let mut memo = HashMap::new();
-    for point in point_list {
-        result += find_paths(point, &map, &mut memo, max_y, max_x);
+    let mut starting_locations = Vec::new();
+    let map: Vec<Vec<isize>> = parse_input(input, &mut starting_locations);
+    let mut trailheads = Vec::new();
+    for point in starting_locations {
+        let hills = get_trailheads(&map, point);
+        trailheads.push(hills);
     }
+    let result = trailheads
+        .iter()
+        .map(|trailhead| trailhead.len() as u32)
+        .sum();
     Some(result)
 }
 
-fn find_paths(
-    point: (isize, isize),
-    map: &Vec<Vec<isize>>,
-    memo: &mut HashMap<(isize, isize), u32>,
-    max_y: usize,
-    max_x: usize,
-) -> u32 {
-    if map[point.0 as usize][point.1 as usize] == 9 {
-        return 1;
-    }
-    if memo.contains_key(&point) {
-        return memo[&point];
-    }
-    let mut result = 0;
-    for (y1, x1) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
-        let new_y = point.0 + y1;
-        let new_x = point.1 + x1;
-
-        if in_bounds(point, map, max_y, max_x, new_y, new_x) {
-            result += find_paths((new_y, new_x), map, memo, max_y, max_x);
+fn get_trailheads(map: &[Vec<isize>], point: (isize, isize)) -> Vec<(isize, isize)> {
+    let max_y = map.len();
+    let max_x = map[0].len();
+    let mut hills = Vec::new();
+    let mut points_to_check = VecDeque::from([point]);
+    while !points_to_check.is_empty() {
+        let point = points_to_check.pop_front().unwrap();
+        if map[point.0 as usize][point.1 as usize] == 9 {
+            hills.push(point);
+        } else {
+            for (y1, x1) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+                let new_y = point.0 + y1;
+                let new_x = point.1 + x1;
+                if in_bounds(point, map, max_x, max_y, new_y, new_x) {
+                    points_to_check.push_back((new_y, new_x));
+                }
+            }
         }
     }
-    memo.insert(point, result);
-    result
+    hills
 }
 
 fn parse_input(input: &str, starting_positions: &mut Vec<(isize, isize)>) -> Vec<Vec<isize>> {
